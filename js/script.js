560f206120c39e6da6a014bde3f7246e85bef129
AdrianaSantorio <adriana.santorio@gmail.com>
2022-01-27 16:37:25 +0100

    connected dot index to current index

--- js/script.js
+++ js/script.js
@@ -6,11 +6,14 @@ Vue.config.devtools = true;
 
 //stampare le foto in pagina
 //stampare in pagina tanti puntini quante sono le immagini
-
+//collegare il current index all' index dell'immagine mostrata
+//creare currentindex
+//spostare il currentIndex dai puntini
 
 const app = new Vue ({
     el: '#root',
     data: {
+        currentIndex: 0,
         images: [
           'images/image1.jpg',
           'images/image2.jpg',
@@ -19,6 +22,15 @@ const app = new Vue ({
         ],
     },
     methods: {
-
+        isActive(index) {
+            return (index === this.currentIndex);
+        },
+        isCurrent(index) {
+            this.currentIndex = index;
+            console.log(index);
+            console.log(this.currentIndex);
+        }
     }
 });
+
+
