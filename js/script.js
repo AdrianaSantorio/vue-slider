e90a6474268e81772cd967c108a4024848db529e
AdrianaSantorio <adriana.santorio@gmail.com>
2022-01-27 15:32:14 +0100

    rendered imgs via js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
 console.log('JS OK');
-
 console.log('Vue ok', Vue);
 
 //per collegarlo con l' estensione
 Vue.config.devtools = true;
+
+//stampare le foto in pagina
+//stampare in pagina tanti puntini quante sono le immagini
+
+
+const app = new Vue ({
+    el: '#root',
+    data: {
+        images: [
+          'images/image1.jpg',
+          'images/image2.jpg',
+          'images/image3.jpg',
+          'images/image4.jpg',
+        ],
+    },
+    methods: {
+
+    }
+});
